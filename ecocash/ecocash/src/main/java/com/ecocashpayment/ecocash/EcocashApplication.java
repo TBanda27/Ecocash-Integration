27a3bc172120f635ac1a1d40d2c3e6a063e6cdcf
TBanda27 <tzbanda@outlook.com>
2026-01-19 21:32:52 +0000

    First Commit

---
+++ ecocash/ecocash/src/main/java/com/ecocashpayment/ecocash/EcocashApplication.java
@@ -0,0 +1,13 @@
+package com.ecocashpayment.ecocash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocashApplication.class, args);
+	}
+
+}
---
+++ ecocash/ecocash/src/test/java/com/ecocashpayment/ecocash/EcocashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecocashpayment.ecocash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
