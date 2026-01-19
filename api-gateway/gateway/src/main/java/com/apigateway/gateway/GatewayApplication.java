b81af002020ec3e7ae039bfcdd9691dc6aa5d996
TBanda27 <tzbanda@outlook.com>
2026-01-19 21:57:37 +0000

    skeleton of the ecocash service, eureka and api-gateway

--- api-gateway/gateway/src/main/java/com/apigateway/gateway/GatewayApplication.java
+++ api-gateway/gateway/src/main/java/com/apigateway/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ecocashpayment.ecocash;
+package com.apigateway.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EcocashApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcocashApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
